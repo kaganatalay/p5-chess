e83d0949b115caef41924d4010544aa17e8175c5
kaganatalay <48818679+kaganatalay@users.noreply.github.com>
2020-12-15 20:59:45 +0300

    board rendering

--- src/board.js
+++ src/board.js
@@ -1,6 +1,16 @@
 class Board {
     constructor() {
         this.cell_size = width / 8;
+        this.position = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ];
     }
 
     start() {
@@ -9,10 +19,22 @@ class Board {
     }
 
     render() {
-
+        for(let i = 0; i < 8; i++) {
+            for(let j = 0; j < 8; j++) {
+                push();
+                noStroke();
+                fill(238, 238, 210);
+                if((i % 2 == 0 && j % 2 != 0) || (i % 2 != 0 && j % 2 == 0)) {
+                    fill(118, 150, 86);  
+                }
+                
+                rect(j * this.cell_size, i * this.cell_size, this.cell_size, this.cell_size);
+                pop();
+            }
+        }
     }
 
     update() {
-        console.log('Game started');
+
     }
 }
