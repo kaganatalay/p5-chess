efdfd1d3be8f11e23c95c105e4ef0221f4466de7
kaganatalay <48818679+kaganatalay@users.noreply.github.com>
2020-12-15 19:37:59 +0300

    created board object

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
+let board;
 function setup() {
   createCanvas(800, 800);
+  board = new Board();
 }
 
 function draw() {
   background(51);
-  
+
+  board.start();
 }
---
+++ src/board.js
@@ -0,0 +1,18 @@
+class Board {
+    constructor() {
+        this.cell_size = width / 8;
+    }
+
+    start() {
+        this.render();
+        this.update();
+    }
+
+    render() {
+
+    }
+
+    update() {
+        console.log('Game started');
+    }
+}
